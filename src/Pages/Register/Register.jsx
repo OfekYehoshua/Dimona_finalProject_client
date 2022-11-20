877ff4449b378084bfd1d0d41355c4d0a1188515
OrReuben <orreuven1243@gmail.com>
2022-11-20 13:43:15 +0200

    added design for register and login

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -4,7 +4,10 @@ import Signup from "../../Components/register/Signup";
 import Login from "../../Components/register/Login";
 import "./Register.css";
 import { AiOutlineRight } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+
 const Register = () => {
+  const navigate = useNavigate();
   return (
     <div className="auth-container">
       <Tabs
@@ -13,7 +16,7 @@ const Register = () => {
         className="mb-3"
       >
         <Tab variant="white" eventKey="הרשמה" title="הרשמה">
-          <div className="return-btn">
+          <div className="return-btn" onClick={() => navigate("/")}>
             <AiOutlineRight />
           </div>
           <div style={{ display: "flex", justifyContent: "center" }}>
@@ -22,6 +25,9 @@ const Register = () => {
         </Tab>
         <Tab eventKey="התחברות" title="התחברות">
           <div style={{ display: "flex", justifyContent: "center" }}>
+            <div className="return-btn" onClick={() => navigate("/")}>
+              <AiOutlineRight />
+            </div>
             <Login />
           </div>
         </Tab>
