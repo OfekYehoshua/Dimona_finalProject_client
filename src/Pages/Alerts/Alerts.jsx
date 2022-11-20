f6c36f8a8e78eda1e42b83f9ebc2395854594a43
Ofek Yehoshua <yehoshoaofek@gmail.com>
2022-11-20 15:56:17 +0200

    connection to server functions

--- src/Pages/Alerts/Alerts.jsx
+++ src/Pages/Alerts/Alerts.jsx
@@ -4,127 +4,114 @@ import axios from "axios";
 import "./alertStyle.css";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 import { useState, useEffect } from "react";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
 const Alerts = () => {
   const user_isAdmin = true;
-  // const [alerts, setAlerts] = useState([]);
+  const [alerts, setAlerts] = useState([]);
   const [title, setTitle] = useState();
   const [subTitle, setSubTitle] = useState();
   const [body, setBody] = useState();
   const [selectedImg, setSelectedImg] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  // const postAlert = async () => {
-  //   try {
-  //     const config = {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         // Authorization: `Bearer ${user.data.token}`,
-  //       },
-  //     };
-  //     const { data } = await axios.post(
-  //       "/api/alerts",
-  //       { title, subTitle, body, selectedImg },
-  //       config
-  //     );
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const [show, setShow] = useState(false);
 
-  // const deleteAlert = async () => {
-  //   try {
-  //     const config = {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         // Authorization: `Bearer ${user.data.token}`,
-  //       },
-  //     };
-  //     const { data } = await axios.delete("/api/alerts/:id", {}, config);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-  // useEffect(() => {
-  //   const getSelectedImg = async () => {
-  //     if (selectedImg) {
-  //       if (
-  //         selectedImg.type === "image/jpeg" ||
-  //         selectedImg.type === "image/png"
-  //       ) {
-  //         const formData = new FormData();
-  //         formData.append("file", selectedImg);
-  //         formData.append("upload_preset", "dimona-citizen-app");
-  //         // setLoading(true);
-  //         await axios
-  //           .post(
-  //             "https://api.cloudinary.com/v1_1/ofekyehoshua/image/upload",
-  //             formData
-  //           )
-  //           .then((res) => setSelectedImg(res.data.secure_url))
-  //           .catch((err) => {
-  //             console.log(err);
-  //           });
-  //         // setLoading(false);
-  //       }
-  //     }
-  //   };
-  //   getSelectedImg();
-  // }, [selectedImg]);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-  // useEffect(() => {
-  //   const fetchAlerts = async () => {
-  //     try {
-  //       const { data } = await axios.get("/api/alerts/");
-  //       setAlerts(...alerts, data);
-  //     } catch (error) {
-  //       console.log("error fetching the alerts");
-  //     }
-  //   };
-  //   fetchAlerts();
-  // }, [alerts]);
-  const alerts = [
-    {
-      _id: 1,
-      createdAt: "2022-02-26T16:37:48.244Z",
-      title: "testss",
-      subTitle: "test2",
-      body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui cumque fugiat ratione dolorem laudantium reiciendis ad ex recusandae nesciunt dolore alias, tempora numquam labore consequatur odit laborum earum. Necessitatibus, quae",
-      img: "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
-    },
-    {
-      _id: 2,
-      title: "test",
-      createdAt: "2022-02-26T16:37:48.244Z",
-      subTitle: "test2",
-      img: "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
-      body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui cumque fugiat ratione dolorem laudantium reiciendis ad ex recusandae nesciunt dolore alias, tempora numquam labore consequatur odit laborum earum. Necessitatibus, quae",
-    },
-    {
-      _id: 3,
-      title: "test",
-      createdAt: "2022-02-26T16:37:48.244Z",
-      subTitle: "test2",
-      img: "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
-      body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui cumque fugiat ratione dolorem laudantium reiciendis ad ex recusandae nesciunt dolore alias, tempora numquam labore consequatur odit laborum earum. Necessitatibus, quae",
-    },
-    {
-      _id: 4,
-      title: "test",
-      subTitle: "test2",
-      createdAt: "2022-02-26T16:37:48.244Z",
-      img: "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
-      body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui cumque fugiat ratione dolorem laudantium reiciendis ad ex recusandae nesciunt dolore alias, tempora numquam labore consequatur odit laborum earum. Necessitatibus, quae",
-    },
-  ];
+  useEffect(() => {
+    const fetchAlerts = async () => {
+      try {
+        await axios
+          .get(`${process.env.REACT_APP_API_URL}/api/alerts`)
+          .then((res) => res.data && setAlerts(res.data));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAlerts();
+  }, []);
+  console.log(alerts);
+  const postAlert = async () => {
+    try {
+      // const config = {
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //     // Authorization: `Bearer ${user.data.token}`,
+      //   },
+      // };
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_API_URL}/api/alerts/`,
+        {
+          title,
+          subTitle,
+          body,
+          img: selectedImg,
+        }
+      );
+      // setAlerts(...alerts, data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteAlert = async (alert) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          // Authorization: `Bearer ${user.data.token}`,
+        },
+      };
+      await axios.delete(
+        `${process.env.REACT_APP_API_URL}/api/alerts/${alert._id}`,
+        config
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    const getSelectedImg = async () => {
+      if (selectedImg) {
+        if (
+          selectedImg.type === "image/jpeg" ||
+          selectedImg.type === "image/png"
+        ) {
+          try {
+            setLoading(true);
+            const formData = new FormData();
+            formData.append("file", selectedImg);
+            formData.append("upload_preset", "dimona-citizen-app");
+            await axios
+              .post(
+                "https://api.cloudinary.com/v1_1/ofekyehoshua/image/upload",
+                formData
+              )
+              .then((res) => setSelectedImg(res.data.secure_url));
+            setLoading(false);
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      }
+    };
+    getSelectedImg();
+  }, [selectedImg]);
 
   return (
     <>
       {alerts ? (
         <>
-          {alerts.map((alert) => (
-            <Card key={alert._id}>
+          {alerts?.map((alert) => (
+            <Card
+              key={alert._id}
+              style={{ display: "flex", flexDirection: "row" }}
+            >
               <img className="alert-img" src={alert.img} alt="img" />
               <div className="card-info">
                 <h1 className="alert-title">{alert.title}</h1>
@@ -132,7 +119,21 @@ const Alerts = () => {
                   {alert.createdAt.split("T")[0]}
                 </span>
               </div>
-              <Button /*onClick={deleteAlert}*/>x</Button>
+              {user_isAdmin ? (
+                <Button
+                  onClick={() => deleteAlert(alert)}
+                  style={{
+                    width: "10vw",
+                    height: "5vh",
+                    position: "relative",
+                    right:'43vw', top:'7vh'
+                  }}
+                >
+                  x
+                </Button>
+              ) : (
+                "none"
+              )}
             </Card>
           ))}
         </>
@@ -147,51 +148,70 @@ const Alerts = () => {
         </>
       )}
       {user_isAdmin ? (
-        <Form
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            margin: "10vh",
-            width: "30vw",
-          }}
-        >
-          <h1>העלאת מבזק </h1>
-          <Form.Group className="mb-3">
-            <Form.Control
-              size="lg"
-              type="text"
-              placeholder="הכנס את הכותרת"
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3">
-            <Form.Control
-              size="md"
-              type="text"
-              placeholder="הכנס את הכותרת משנה"
-              onChange={(e) => setSubTitle(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Control
-              size="lg"
-              as="textarea"
-              placeholder="הכנס את גוף הכתבה"
-              onChange={(e) => setBody(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Control
-              type="file"
-              accept="application/pdf, image/png"
-              onChange={(e) => setSelectedImg(e.target.files[0])}
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit" /*onClick={postAlert}*/>
-            העלה
+        <>
+          <Button variant="primary" onClick={handleShow}>
+            הוסף מבזק
           </Button>
-        </Form>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header>
+              <Modal.Title>העלאת מבזק</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <Form.Group className="mb-3">
+                  <Form.Control
+                    size="lg"
+                    type="text"
+                    placeholder="הכנס את הכותרת"
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Form.Control
+                    size="md"
+                    type="text"
+                    placeholder="הכנס את הכותרת משנה"
+                    onChange={(e) => setSubTitle(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Control
+                    size="lg"
+                    as="textarea"
+                    placeholder="הכנס את גוף הכתבה"
+                    onChange={(e) => setBody(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Control
+                    type="file"
+                    accept="application/pdf, image/png"
+                    onChange={(e) => setSelectedImg(e.target.files[0])}
+                  />
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                סגור
+              </Button>
+              <Button
+                variant="primary"
+                type="button"
+                disabled={loading}
+                onClick={postAlert}
+              >
+                העלה
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
       ) : (
         "none"
       )}
